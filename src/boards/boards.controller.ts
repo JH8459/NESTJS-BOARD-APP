3761bb7f6794f7a896142cbf656b8d95f07665b9
JH8459 <kk_ong2233@naver.com>
2022-04-16 02:19:39 +0900

    Done: 게시물 생성하기 (Controller 부분)

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 
@@ -11,5 +11,14 @@ export class BoardsController {
   getAllBoard(): Board[] {
     // Board[] 형식으로 데이터 타입 선언
     return this.boardsService.getAllBoards(); // boardsService 안에서 만든 getAllBoards() 메서드를 호출한다
+  }
+
+  @Post() // Post 요청 메서드일 경우
+  /* 게시물을 생성하는 핸들러(@Body body를 통해 요청에서 보내온 모든 body값을 가져올 수도 있다) */
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ) {
+    return this.boardsService.createBoard(title, description);
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
@@ -8,5 +9,17 @@ export class BoardsService {
   getAllBoards(): Board[] {
     // Board[] 형식으로 리턴값의 데이터 형식 선언
     return this.boards; // 모든 boards를 가져오는 Service
+  }
+  /* 전달인자로 title과 description을 갖는 보드를 생성하는 createBoard Service 선언 */
+  createBoard(title: string, description: string) {
+    /* 만들어질 board는 Board 모델을 타입을 따른다 */
+    const board: Board = {
+      id: uuid(), // uuid 모듈을 이용한 유니크한 ID값 선언
+      title, // title: title,
+      description, // description: description,
+      status: BoardStatus.PUBLIC, // status값은 BoardStatus.PUBLIC으로 초기화한다
+    };
+    this.boards.push(board); // boards 배열에 지금 board를 push 한다
+    return board; // 현재 전달인자(title, description)값으로 만들어진 board를 생성한다
   }
 }
