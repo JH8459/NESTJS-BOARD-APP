884a83cb1c5f94dd4429dd378df42c679d1e104e
JH8459 <kk_ong2233@naver.com>
2022-04-18 14:14:00 +0900

    Done: TypeORM 이용

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+/* TypeORM Config */
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: '8459',
+  database: 'board_app',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true,
+};
