f3dce06aff45c5d0503a76144a85c8e7853e00f4
JH8459 <kk_ong2233@naver.com>
2022-04-18 11:19:43 +0900

    Edit: 커스텀 파이프를 이용한 유효성 체크

--- src/boards/pipes/board-status-validation.pipe.ts
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -1,8 +1,4 @@
-import {
-  ArgumentMetadata,
-  BadRequestException,
-  PipeTransform,
-} from '@nestjs/common';
+import { BadRequestException, PipeTransform } from '@nestjs/common';
 import { BoardStatus } from '../board.model';
 
 /* BoardStatusValidationPipe은 status가 'PUBLIC', 'PRIVATE'로만 설정될 수 있도록 하는 커스텀 파이프 생성 */
@@ -10,7 +6,7 @@ export class BoardStatusValidationPipe implements PipeTransform {
   readonly StatusOptions = [BoardStatus.PRIVATE, BoardStatus.PUBLIC]; // 읽기전용 StatusOptions 배열 선언
 
   transform(value: any) {
-    /* ※ value는 인풋값 그 자체이며, metadata는 타입과 파라미터에 대한 정보를 담고 있다 */
+    /* ※ value는 인풋값 그 자체이며, metadata는 타입과 파라미터에 대한 정보를 담고 있다 (metadata는 사용하지 않았으므로 생략) */
     value = value.toUpperCase(); // 인풋값으로 받은 value를 대문자로 변환한다
     /* isStatusValid 유효성 검사를 value값을 넣어 통과하지 못한다면 유효하지 않은 value 값이므로 에러메시지를 던진다 */
     if (!this.isStatusValid(value)) {
