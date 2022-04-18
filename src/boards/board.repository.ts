0810150f08803d3a6a05060ecf0aafa845817ce0
JH8459 <kk_ong2233@naver.com>
2022-04-18 22:28:37 +0900

    Edit: 게시물 생성하기

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -6,16 +6,16 @@ import { CreateBoardDto } from './dto/create-board.dto';
 /* Board Entity를 컨트롤하는 Repository를 선언 */
 @EntityRepository(Board)
 export class BoardRepository extends Repository<Board> {
+  /* createBoard 메서드 중 DB 관련 동작은 Repository에서 수행한다 */
   async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
     /* title과 description은 구조분해 할당으로 createBoardDto에서 꺼내어서 사용해준다 */
     const { title, description } = createBoardDto;
-    /* 만들어질 board는 Board 모델을 타입을 따른다 */
     const board = this.create({
       title, // title: title,
       description, // description: description,
       status: BoardStatus.PUBLIC, // status값은 BoardStatus.PUBLIC으로 초기화한다
     });
-    await this.save(board); // boardRepository에 지금 board를 데이터베이스에 save한다
-    return board; // 전달인자(title, description)값으로 만들어진 board를 생성한다
+    await this.save(board); // board를 데이터베이스에 저장
+    return board; // DB에 저장한 board를 반환한다
   }
 }
